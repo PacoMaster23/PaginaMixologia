39e9e3355808b9d1cc9908da38d879dc96d316f0
PacoMaster23 <francizco560@hotmail.com>
2025-10-27 20:54:29 -0600

    Implementacion de HTML, Controlador y Vista

---
+++ ML/Solicitudes.cs
@@ -0,0 +1,20 @@
+﻿namespace ML
+{
+    public class Solicitudes
+    {
+        public int IdSolicitud { get; set; }
+
+        public string? NombreUsuario { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Telefono { get; set; }
+
+        public string? Mensaje { get; set; }
+
+        public DateTime? FechaSolicitud { get; set; }
+
+        public ML.StatusSolicitudes? StatusSolicitudes { get; set; }
+
+    }
+}
---
+++ Mixologia/Controllers/CocteleriaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mixologia.Controllers
+{
+    public class CocteleriaController : Controller
+    {
+        private readonly BL.Solicitudes _contextSolicitudes;
+        public CocteleriaController(BL.Solicitudes context)
+        {
+            _contextSolicitudes = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Mixologia()
+        {
+            return View();
+        }
+    }
+}
