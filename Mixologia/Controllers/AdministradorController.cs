55ca944f1e99c364ae9a989a96dd191980fb860e
FranciscoBatalla <falfonsob01@outlook.com>
2025-10-31 09:58:27 -0600

    Agrego metodo GetAll

--- Mixologia/Controllers/AdministradorController.cs
+++ Mixologia/Controllers/AdministradorController.cs
@@ -4,13 +4,33 @@ namespace Mixologia.Controllers
 {
     public class AdministradorController : Controller
     {
+
+    private readonly BL.Solicitudes _context
+    public AdministradorController(BL.Solicitud context)
+    {
+    _context = context;
+    }
+    
         public IActionResult Index()
         {
             return View();
         }
+        
+        [HttpGet]
         public IActionResult Solicitudes()
         {
-            return View();
+        ML.Solicitud Solicitud = new ML.Solicitud();
+        ML.Result resultSolicitud = new ML.Result();
+
+        if(resultSolicitud.Correct)
+        {
+        Solicitud.Solicitudes = resultSolicitud.Objects;
         }
+        
+            return View(Solicitud);
+        }
+
+
+        
     }
 }
